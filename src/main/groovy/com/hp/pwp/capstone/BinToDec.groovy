280d55f7b73c50fcd00f2257eeb2ef5e9b96961c
grocki <grocki@localhost.localdomain>
2019-01-23 18:28:20 -0800

    beanstalk works

--- src/main/groovy/com/hp/pwp/capstone/BinToDec.groovy
+++ src/main/groovy/com/hp/pwp/capstone/BinToDec.groovy
@@ -1,8 +1,11 @@
 package com.hp.pwp.capstone
-import com.surftools.BeanstalkClient
+import com.surftools.BeanstalkClientImpl.ClientImpl
+import com.surftools.BeanstalkClientImpl.JobImpl
+import com.surftools.BeanstalkClient.BeanstalkException
 
 class toBin {
 
+	
 	 int binaryToInteger(int binString) {
 		int asciiValue=0;
 
@@ -43,6 +46,16 @@ class toBin {
 
 
 	public static void main(String [] args) {
+	
+	long priority = 0;
+	int delaySeconds = 0;
+	int timeToRun = 10;
+	byte[] data = "hey";
+	ClientImpl connection = new ClientImpl("localhost", 11300);
+	connection.put(priority, delaySeconds, timeToRun, data);
+	
+	JobImpl job = connection.reserve();
+	println job.data;
 
         while(true) {
 
