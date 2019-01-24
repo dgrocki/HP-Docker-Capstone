5ef5fcf4a69f717fb945eb137fb6798759630e8a
Austin Sanders <sanderau@oregonstate.edu>
2019-01-23 17:05:26 -0800

    Updated the unit tests

--- src/test/groovy/com/hp/pwp/capstone/BinToDecTest.groovy
+++ src/test/groovy/com/hp/pwp/capstone/BinToDecTest.groovy
@@ -8,7 +8,7 @@ public class BinToDecTest extends GroovyTestCase {
 	Random rnd = new Random();
 	def b = new toBin();
 
-	for(int i = 0; i < 1000000; i++)
+	for(int i = 0; i < 100; i++)
 	{
 		String input = "";
 
@@ -17,31 +17,67 @@ public class BinToDecTest extends GroovyTestCase {
 			input = input + rnd.nextInt(2) as String;
 		}
 
-//		assert(b.isBinary(input));
+	    assert(b.isBinary(input))
 	}
 
 	
 	}
 
-   void testBintoIntBad() {
-	Random rnd = new Random();
-	def b = new toBin();
+   void testAlpha() {
+        Random rnd = new Random()
+	    def b = new toBin()
+        String alpha = ""
 
-	for(int i = 0; i < 1000000; i++)
-	{
-		String input = "";
+        for(int i = 0; i < 8; i++) 
+        {
+            alpha = alpha + ((rnd.nextInt(27) + 97) as char)
+        }
 
-		for(int j = 0; j < 8; j++)
-		{
-			int add = rnd.nextInt(8) + 2;
-			input = input + add as String;
-		}
+	
+        assert(!(b.isBinary(alpha)))
+    }
 
-//		assert(b.isBinary(input) == false);
-	}
+    void testBigNegative() {
+        Random rnd = new Random()
+        def b = new toBin()
+        int bigNegative
 
-        assert(true)
-	}
+        bigNegative = (rnd.nextInt(100000) + 9)*-1
+
+        assert(!(b.isBinary(bigNegative as String)))
+    }
+
+    void testBigPositive() {
+        Random rnd = new Random()
+        def b = new toBin()
+        int bigPositive
+
+        bigPositive = (rnd.nextInt(100000) + 9)
+
+        assert(!(b.isBinary(bigPositive as String)))
+    }
+
+    void testSmallNegative () {
+        Random rnd = new Random()
+        def b = new toBin()
+        int smallNegative
+
+        smallNegative = (rnd.nextInt(6) + 1)*-1
+
+        assert(!(b.isBinary(smallNegative as String)))
+
+    }
+
+    void testSmallPositive() {
+        Random rnd = new Random()
+        def b = new toBin()
+        int smallPositive
+
+        smallPositive = (rnd.nextInt(6) + 1)
+
+        assert(!(b.isBinary(smallPositive as String)))
+
+    }
  
 
 }
