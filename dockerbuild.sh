d3c7c5f87d2304d1c0478ec426b9c920e5d99970
Brendan Byers <b11484@gmail.com>
2019-02-07 10:16:40 -0800

    Wrapped cp path in quotes because of spaces

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-cp ${WORKSPACE}/build/libs/binToDec-all.jar ./
+cp "${WORKSPACE}/build/libs/binToDec-all.jar" ./
 
 docker build -t iceberg00/hp-docker-capstone:latest -t iceberg00/hp-docker-capstone:${BUILD_NUMBER} .
-docker login -u DOCKER_CREDS_USR -p DOCKER_CREDS_PSW
+docker login -u iceberg00 -p DOCKER_CREDS_PSW
 docker push iceberg00/hp-docker-capstone:${BUILD_NUMBER}
 docker push iceberg00/hp-docker-capstone:latest
-
-rm -r ./dockerFiles
