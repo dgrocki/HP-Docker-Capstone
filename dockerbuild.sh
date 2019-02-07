c2ee2a1349e8c216c8def3016a2d9ae129df52e3
Brendan Byers <b11484@gmail.com>
2019-02-07 09:54:24 -0800

    Changed path to dockerfile and jar file

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 mkdir dockerFiles
-cp ./build/libs/binToDec-all.jar ./dockerFiles/binToDec-all.jar
+cp ${WORKSPACE}/build/libs/binToDec-all.jar ./dockerFiles/binToDec-all.jar
 
-docker build -t iceberg00/hp-docker-capstone:latest -t iceberg00/hp-docker-capstone:${BUILD_NUMBER} ./dockerFiles
+docker build -t iceberg00/hp-docker-capstone:latest -t iceberg00/hp-docker-capstone:${BUILD_NUMBER} ${WORKSPACE}
 docker login -u DOCKER_CREDS_USR -p DOCKER_CREDS_PSW
 docker push iceberg00/hp-docker-capstone:${BUILD_NUMBER}
 docker push iceberg00/hp-docker-capstone:latest
