292be5d9251199986ee961b2d5abac1e05c7498f
Brendan Byers <b11484@gmail.com>
2019-02-07 10:24:01 -0800

    added debugging ls

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cp "${WORKSPACE}/build/libs/binToDec-all.jar" ./
+ls
+cp "./build/libs/binToDec-all.jar" ./
 
 docker build -t iceberg00/hp-docker-capstone:latest -t iceberg00/hp-docker-capstone:${BUILD_NUMBER} .
 docker login -u iceberg00 -p DOCKER_CREDS_PSW
