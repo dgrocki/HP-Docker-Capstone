59c1cd8c79265cdcaa63d10469e0e6c02592636d
loughrao <loughrao@oregonstate.edu>
2019-04-13 16:35:51 -0700

    Testing kubernetes

--- MultiTest/setup.sh
+++ MultiTest/setup.sh
@@ -3,6 +3,9 @@
 #Dependencies: 
 #* wget
 #* git
+#* kernel-devel
+#* kernel-headers
+#* kernel-devel.x86_64 0:3.10.0-957.eludo 
 
 #Order of operations:
 #* install docker
@@ -34,6 +37,19 @@ sudo yum install -y VirtualBox-6.0
 sudo /usr/lib/virtualbox/vboxdrv.sh setup
 sudo usermod -a -G vboxusers $USER
 
+#* Install kubectl
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo yum install -y kubectl
 
-
+#* Install minikube
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+sudo cp minikube /usr/local/bin && rm minikube
 
